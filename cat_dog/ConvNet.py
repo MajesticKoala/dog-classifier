2969555d6663dfb32f932c999b9f6e1244ac2ebc
Tom <tomas.walshy@gmail.com>
2020-07-19 21:18:38 +1000

    Improved documentation in python program

--- cat_dog/ConvNet.py
+++ cat_dog/ConvNet.py
@@ -6,19 +6,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 
+#Set up TensorBoard
 log_path = f'logs/cats-vs-dogs-cnn-64x2-{int(time.time())}'
-
 tensorboard = TensorBoard(log_dir=log_path, histogram_freq=1)
 
 X = np.load('features.npy')
 y = np.load('labels.npy')
 
+#Example image
 #print(y[515])
 #plt.imshow(X[515].reshape(60,60), cmap='gray')
 #plt.show()
 
-X = X/255.0
+X = X/255.0 #Normalize the imageto value between 0-1
 
+#Create layers to neural net
 model = Sequential()
 
 model.add(Conv2D(64, (3,3), input_shape = X.shape[1:]))
@@ -30,8 +32,8 @@ model.add(Activation("relu"))
 model.add(MaxPooling2D(pool_size=(2,2)))
 
 model.add(Flatten())
-model.add(Dense(64))
-model.add(Activation("relu"))
+#model.add(Dense(64))
+#model.add(Activation("relu"))
 
 model.add(Dense(1))
 model.add(Activation("sigmoid"))
@@ -40,4 +42,5 @@ model.compile(loss="binary_crossentropy",
              optimizer="adam",
              metrics=['accuracy'])
 
-model.fit(X, y, batch_size=12, epochs=1, validation_split=0.1, callbacks=[tensorboard])
+#Train model
+model.fit(X, y, batch_size=12, epochs=3, validation_split=0.1, callbacks=[tensorboard])
