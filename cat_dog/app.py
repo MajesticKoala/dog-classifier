ca8d320b0c73b4498c51145c53719ee0d41a2264
Tom <tomas.walshy@gmail.com>
2020-07-25 14:19:30 +1000

    Front end working

--- cat_dog/app.py
+++ cat_dog/app.py
@@ -2,12 +2,9 @@ from flask import Flask, render_template, request, redirect, flash
 from testModel import load_model, prepare
 import numpy as np
 import matplotlib.pyplot as plt
-import cv2
-import sys
+import cv2, sys, os, shutil
 from PIL import Image
-import os
 import tensorflow as tf
-import base64
 
 app = Flask(__name__)
 
@@ -19,30 +16,45 @@ def index():
     return render_template('index.html')
 
 def predict(image_array):
-    CATEGORIES = ['Dog', 'Cat']
+    CATEGORIES = ['Dog 🐶', 'Cat 🐱']
 
     model = tf.keras.models.load_model("64x3-CNN-70px.model")
     prediction = model.predict([image_array])
     return CATEGORIES[int(prediction[0][0])]
 
+def clear_directory():
+    folder = 'images'
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e), file=sys.stderr)
+
 @app.route('/upload', methods=['POST'])
 def add_image():
+    #First clear image directory
+    clear_directory()
     if request.files:
         in_image = request.files["image"]
         image = in_image.read()
         image_name = in_image.filename
 
+        print(image_name, file=sys.stderr)
+
         if image_name == '':
             return redirect('/')
 
-        print(image_name, file=sys.stderr)
-
+        #Create image arrays
         npimg = np.fromstring(image, np.uint8)
-        base64img = base64.b64encode(npimg)
         img_array = cv2.imdecode(npimg, cv2.IMREAD_GRAYSCALE)
         big_array = cv2.resize(img_array, (200, 200))
         new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
 
+        #Create images from arrays and save in images folder
         img_print = Image.fromarray(big_array)
         img_predict = Image.fromarray(new_array)
         img_predict.save(os.path.join(UPLOAD_FOLDER, image_name))
@@ -50,7 +62,7 @@ def add_image():
 
         prediction = predict(prepared_img)
 
-    return render_template('index.html', predict=prediction, img_data=base64img )
+    return render_template('index.html', predict=prediction, file_name=image_name )
 
 if __name__ == "__main__":
     app.run(debug=True)
